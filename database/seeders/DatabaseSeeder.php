86d586c1230c5b1077012cac3e0bc41170c65912
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2020-11-20 09:53:15 +0100

    Seeder pt. 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\ScenarioSeeder;
+use Database\Seeders\SceneSeeder;
+use Database\Seeders\ItemSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ScenarioSeeder::class,
+            SceneSeeder::class,
+            ItemSeeder::class,
+        ]);
     }
 }
