03ebcfb3002eaf1818c040f34112a43d5f280072
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2020-11-20 16:15:03 +0100

    Création des models

---
+++ app/Models/SavedItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SavedItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'saved_items';
+    public $timestamps = false;
+}
---
+++ app/Models/SavedScene.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SavedScene extends Model
+{
+    use HasFactory;
+
+    protected $table = 'saved_scenes';
+    public $timestamps = false;
+}
